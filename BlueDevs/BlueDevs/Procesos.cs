a2bf6e8a10a0ad8911ab5f43f3c28d8ecabe0705
Pablo Alvarado <pablo.andre_alvarado@hotmail.com>
2020-02-01 09:35:52 -0600

    Segundo Problema
    Se ha implementado el segudno problema.

---
+++ BlueDevs/BlueDevs/Procesos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace BlueDevs
+{
+    public class Procesos
+    {
+
+        public void SerieFibonacci(int num, ref string cadena)
+        {
+            if (num == 0)
+                cadena = "";
+            else if (num == 1)
+                cadena = "0";
+            else if (num == 2)
+                cadena = "0, 1";
+            else
+            {
+                cadena = "0, 1";
+                num = num - 2;
+                SerieFibonacci(0, 1, num, ref cadena);
+            }
+                
+        }
+
+        private static void SerieFibonacci(int num1, int num2, int num, ref string cadena)
+        {
+            int tempVal = num1 + num2;
+            cadena += ", " + tempVal;
+            num--;
+
+            if (num > 0)
+                SerieFibonacci(num2, tempVal, num, ref cadena);
+        }
+    }
+}
---
+++ BlueDevs/BlueDevs/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace BlueDevs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var seleccion = 0;
+            Procesos proc = new Procesos();
+
+            while (seleccion != 5)
+            {
+                Console.Clear();
+                Console.WriteLine("Bienvenido\n Ingrese el numero del problema que quiere seleccionar.\n\n" +
+                    "2. Problema 2\n" +
+                    "3. Problema 3\n" +
+                    "4. Problema 4\n" +
+                    "5. Salir");
+
+                try
+                {
+                    seleccion = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine();
+
+                    if (seleccion == 2)
+                    {
+                        int num;
+
+                        do
+                        {
+                            Console.WriteLine("PROBLEMA 2\n" +
+                                "Ingrese un número entre 0 y 15");
+
+                            try
+                            {
+                                num = Convert.ToInt16(Console.ReadLine());
+
+                                string resultado = "";
+
+                                if (num >= 0 && num <= 15)
+                                {
+                                    proc.SerieFibonacci(num, ref resultado);
+                                    Console.WriteLine("Serie de Fibonacci: " + resultado);
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Ingrese un número válido\n");
+                                }
+
+                            }
+                            catch
+                            {
+                                num = -1;
+                                Console.WriteLine("Ingrese un caracter válido\n");
+                            }
+
+
+                        } while (num < 0 || num > 15);
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+
+                    }
+                    else if (seleccion == 3)
+                    {
+                        Console.WriteLine("PROBLEMA 3");
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                    }
+                    else if (seleccion == 4)
+                    {
+                        Console.WriteLine("PROBLEMA 4");
+
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                    }
+                }
+                catch
+                {
+
+                }
+            }
+            
+        }
+
+        
+
+        
+
+
+
+
+    }
+}
